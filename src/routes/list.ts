fe4b33d10b662794e8277c5311061d733ed1ac1b
Felipe <felipecongioalbino11@gmail.com>
2025-06-05 20:02:59 -0300

    feat: added basic swagger documentation

--- src/routes/list.ts
+++ src/routes/list.ts
@@ -2,14 +2,117 @@ import {Router} from 'express'
 import listController from '../controller/listController';
 const routes = Router();
 
+/*
+
+      "get": {
+        "description": "",
+        "responses": {
+          "200": {
+            "description": "OK"
+          }
+        }
+      
+*/
 routes.get('/',listController.list);
 
+/*
+"post": {
+        "description": "",
+        "responses": {
+          "201": {
+            "description": "Created"
+          },
+          "500": {
+            "description": "Internal Server Error"
+          }
+        }
+      }
+*/
 routes.post('/',listController.create);
 
+
+/*
+"/lists/{id}": {
+      "get": {
+        "description": "",
+        "parameters": [
+          {
+            "name": "id",
+            "in": "path",
+            "required": true,
+            "type": "string"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "OK"
+          },
+          "404": {
+            "description": "Not Found"
+          },
+          "500": {
+            "description": "Internal Server Error"
+          }
+        }
+      }
+ */
 routes.get('/:id',listController.findById);
+
+/**
+ * "put": {
+        "description": "",
+        "parameters": [
+          {
+            "name": "id",
+            "in": "path",
+            "required": true,
+            "type": "string"
+          },
+          {
+            "name": "body",
+            "in": "body",
+            "schema": {
+              "type": "object",
+              "properties": {
+                "name": {
+                  "example": "any"
+                }
+              }
+            }
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "OK"
+          }
+        }
+      }
+ */
 
 routes.put('/:id',listController.update);
 
+
+/**
+ * "delete": {
+        "description": "",
+        "parameters": [
+          {
+            "name": "id",
+            "in": "path",
+            "required": true,
+            "type": "string"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "OK"
+          },
+          "500": {
+            "description": "Internal Server Error"
+          }
+        }
+      }
+ */
 routes.delete('/:id',listController.delete);
 
 
--- src/routes/task.ts
+++ src/routes/task.ts
@@ -3,17 +3,149 @@ import taskController from '../controller/taskController';
 
 const routes = Router();
 
+
+/**
+ * "get": {
+        "description": "",
+        "responses": {
+          "200": {
+            "description": "OK"
+          }
+        }
+      }
+ */
 routes.get('/',taskController.list);
 
+/**
+ * "post": {
+        "description": "",
+        "responses": {
+          "201": {
+            "description": "Created"
+          },
+          "500": {
+            "description": "Internal Server Error"
+          }
+        }
+      }
+ */
 routes.post('/',taskController.create);
 
+/**
+ * "/tasks/{id}": {
+      "get": {
+        "description": "",
+        "parameters": [
+          {
+            "name": "id",
+            "in": "path",
+            "required": true,
+            "type": "string"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "OK"
+          },
+          "500": {
+            "description": "Internal Server Error"
+          }
+        }
+      }
+ */
 routes.get('/:id',taskController.findById);
 
+
+/**
+ * "put": {
+        "description": "",
+        "parameters": [
+          {
+            "name": "id",
+            "in": "path",
+            "required": true,
+            "type": "string"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "OK"
+          },
+          "500": {
+            "description": "Internal Server Error"
+          }
+        }
+      }
+ */
 routes.put('/:id',taskController.update);
 
+/**
+ * "delete": {
+        "description": "",
+        "parameters": [
+          {
+            "name": "id",
+            "in": "path",
+            "required": true,
+            "type": "string"
+          }
+        ],
+        "responses": {
+          "default": {
+            "description": ""
+          }
+        }
+      }
+ */
 routes.delete('/:id',taskController.delete);
 
+
+/**
+ * "/tasks/listId/{listId}": {
+      "get": {
+        "description": "",
+        "parameters": [
+          {
+            "name": "listId",
+            "in": "path",
+            "required": true,
+            "type": "string"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "OK"
+          },
+          "500": {
+            "description": "Internal Server Error"
+          }
+        }
+      }, 
+    }
+ */
 routes.get('/listId/:listId', taskController.findByListId);
+
+/**
+ * 
+ * "/tasks/listId/{listId}": {
+      "delete": {
+        "description": "",
+        "parameters": [
+          {
+            "name": "listId",
+            "in": "path",
+            "required": true,
+            "type": "string"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "OK"
+          }
+        }
+      }
+    }
+ */
 
 routes.delete('/listId/:listId',taskController.deleteByListId)
 
